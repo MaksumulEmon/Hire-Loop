3d17901db873348b4a08db7a689ab90ebaf2f17e
MaksumulEmon <maksumulemon300@gmail.com>
2026-06-09 15:05:48 +0600

    Module 58 done

--- src/lib/actions/jobs.js
+++ src/lib/actions/jobs.js
@@ -1,15 +1,10 @@
+
+
 'use server'
 
-const baseUrl = process.env.
-    NEXT_PUBLIC_BASE_URL;
-export const createJob = async (newJobData) => {
-    const res = await fetch(`${baseUrl}/api/jobs`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newJobData),
-    });
+import { serverMutation } from "../core/server";
 
-    return res.json();
+
+export const createJob = async (newJobData) => {
+    return serverMutation('/api/jobs', newJobData);
 }
